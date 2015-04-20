ba1a8099b26c48cedbe7b558ab72ec49ef4f185a
Yu <yt2446@columbia.edu>
2015-04-19 20:27:30 -0400

    add hierarchical clustering r script

---
+++ scripts/hierarchical_clustering.R
@@ -0,0 +1,10 @@
+setwd("~/Documents/QMSS/2015spring/DataViz/4NYDine/data/")
+features_mtx <- read.csv("business_features_mtx.csv",head=F)
+features_mtx$V1 <- seq(1,nrow(features_mtx),1)
+refer <-  read.csv("business_refer.csv",head=F)
+refer <- t(refer)
+
+d <- dist(as.matrix(data[,c(-1,-2,-3)]))
+hc <- hclust(d) 
+hc$labels <- refer
+plot(hc, main="Hierarchical Clustering of 50 businesses",cex=.75)
